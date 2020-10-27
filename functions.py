378bc3325f6f46667d14d576122bdf8edeb240a8
“p-szymo” <joshua.szymanowski@gmail.com>
2020-10-27 13:58:18 -0400

    add syllable_counter and unique_word_counter functions

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import string
+import pronouncing as pr
 
 
 def string_adder(source, orig, start_index, end_index):
@@ -220,3 +222,148 @@ def word_counter(lines):
     word_count = sum(line_count)
 
     return word_count
+
+
+def unique_word_counter(lines):
+
+    '''
+    Function to count the number of unique words in a list of
+    strings.
+
+
+    Input
+    -----
+    lines : list (str)
+        List of strings to count.
+
+
+    Output
+    ------
+    num_unique : int
+        Total number of unique words across all strings.
+
+    '''
+
+    # lowercase all words, remove punctuation and en dashes
+    process_lines = [line.lower().replace('—', ' ').
+                     translate(str.maketrans('', '', string.punctuation))
+                     for line in lines]
+
+    # list of all words
+    words = [word for line in process_lines
+             for word in line.split()]
+
+    # number of unique words
+    num_unique = len(set(words))
+
+    return num_unique
+
+
+# count number of end rhymes in a text
+def end_rhyme_counter(lines):
+
+    '''
+    Function to count the instances of rhymes that occur among
+    the last words in lines (end rhymes).
+
+
+    Input
+    -----
+    lines : list (str)
+        List of strings to compare.
+
+
+    Output
+    ------
+    sum(rhyme_counts) : int
+        The number of end rhymes.
+
+    '''
+
+    # instantiate an empty dictionary
+    rhyme_dict = {}
+
+    # make a list of words at the end of the line
+    end_words = [line.split()[-1].translate
+                 (str.maketrans('', '', string.punctuation))
+                 for line in lines]
+
+    # loop to build the dictionary
+    for word in end_words:
+        for i in range(len(end_words)):
+
+            # check if a word rhymes with another word in the list
+            if end_words[i] in pr.rhymes(word):
+
+                # check if word is already a key in the dictionary
+                if word not in rhyme_dict:
+
+                    # or if its rhyming word is already a key
+                    # in the dictionary
+                    if end_words[i] not in rhyme_dict:
+
+                        # if neither is, create the word as key and
+                        # it's rhyme as a value (in a list)
+                        rhyme_dict[word] = [end_words[i]]
+
+                else:
+                    # if word is already a key, append its rhyme to
+                    # its value
+                    rhyme_dict[word].append(end_words[i])
+
+    # count up the amount of (unique) rhymes per word
+    rhyme_counts = [len(rhyme) for rhyme in rhyme_dict.values()]
+
+    return sum(rhyme_counts)
+
+
+# count total syllables in text
+def syllable_counter(lines):
+
+    '''
+    Function to count all syllables in a list of strings.
+
+    NOTE: This does not factor in multi-syllabic digits,
+    times (i.e. 1:03), and most likely other non-"word" words.
+
+
+    Input
+    -----
+    lines : list (str)
+        List of strings to count.
+
+
+    Output
+    ------
+    sum(total) : int
+        Total number of syllables in the input list.
+
+
+
+    [Modified from Allison Parrish's example in the documention
+     for her library, pronouncing]:
+    https://pronouncing.readthedocs.io/en/latest/tutorial.html
+
+    '''
+    # create empty list
+    total = []
+
+    # loop over list
+    for line in lines:
+
+        # turn each word into a string of its phonemes
+        # if else statement ensures that each word is counted with
+        # at least one syllable, even if that word is not in the
+        # pronouncing library's dictionary (using phoneme for 'I'
+        # as a placeholder for single syllable)
+        phonemes = [pr.phones_for_word(word)[0]
+                    if pr.phones_for_word(word)
+                    else 'AY1' for word in line.split()]
+
+        # count the syllables in each string and add the total
+        # syllables per line to the total list
+        total.append(sum([pr.syllable_count(phoneme)
+                          for phoneme in phonemes]))
+
+    # return the total number of syllables
+    return sum(total)
